3bebec952ee952050ab75fc5933f074c01809981
dezlymacauley <dezlymacauley@proton.me>
2024-02-06 14:14:14 +0200

    Project completed

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,55 @@
 use std::io;
-// This calls io from the standard library (std)
+use std::io::Write; // This is needed to use the flush method
+use rand::Rng;
+use std::cmp::Ordering;
+
 fn main() {
+    
+    // NOTE: Let the player know how the game works 
+    
     println!("Guess the secret number");
-    println!("Hint: The number is between 1 and 100 (inclusive)");
+    println!("Hint: The number is between 1 and 50 (inclusive)");
     println!("===============================");
     
-    // `mut` because I want the user to be able to change their guess.
-    // `new` is an associated function of the String type
-    let mut guess = String::new();
+    // NOTE: Generate the secret number
+
+    let secret_number = rand::thread_rng().gen_range(1..=60);
    
-    // This calls stdin from the io module  
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read guess");
-    println!("You guessed: {}", guess);
+    // NOTE: Create a loop that will allow the player to keep guessing
+
+    loop {
+        
+        // NOTE: Allow the user to enter their guess then store it
+
+        print!("Enter your guess: ");
+        io::stdout().flush().expect("Failed to flush");
+        // This wil make sure the prompt is displayed like this:
+        // Enter your guess: 18 
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read guess");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+
+        // NOTE: Check if the player's guess was correct
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("The guess was too low."),
+            Ordering::Greater => println!("Your guess was too high."),
+            Ordering::Equal=> {
+            println!("Your guess was correct!");
+            println!("The secret number was: {}", secret_number);
+            break;
+            }
+        }
+    }
     
-    // NOTE: How readline works
-
-    // The full job of read_line,
-    // is to take whatever the user types into standard input,
-    // and append that into a string (without overwriting its contents),
-    // so we therefore pass that string as an argument.
-    // The string argument needs to be mutable, 
-    // so the method can change the string’s content.
-
 }
